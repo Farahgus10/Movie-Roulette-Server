9b3566bedc6bbd23efb6bda33c41b0dd2335c457
Farah Gustafson <farah.gus@hotmail.com>
2021-07-12 17:24:36 -0400

    changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,12 +3,6 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    }
-    // "connectionString": process.env.DATABASE_URL,
-    // "ssl": { 
-    //   rejectUnauthorized: false 
-    // } 
+    "connectionString": process.env.DATABASE_URL,
+    "ssl": true
   }
