16086cc577604459fc95d033806ebce2c0e8bb61
Farah Gustafson <farah.gus@hotmail.com>
2021-07-12 15:14:44 -0400

    changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,8 +4,8 @@ module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": process.env.DATABASE_URL,
-    "host": process.env.MIGRATION_DB_HOST,
-    "database":  process.env.MIGRATION_DB_NAME,
-    "username": process.env.MIGRATION_DB_USER,
+    // "host": process.env.MIGRATION_DB_HOST,
+    // "database":  process.env.MIGRATION_DB_NAME,
+    // "username": process.env.MIGRATION_DB_USER,
     "ssl": true
   }
