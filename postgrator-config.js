d1b23d9d956ae4f36ace65ff10baa940ec9be452
Farah Gustafson <farah.gus@hotmail.com>
2021-07-12 18:46:37 -0400

    changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const { Pool } = require('pg');
 
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": process.env.DATABASE_URL,
+    "ssl": true
   }
--- src/config.js
+++ src/config.js
@@ -5,10 +5,5 @@ module.exports = {
     // DB_URL: process.env.DB_URL || 'postgresql://movie_roulette_user@localhost/movie_roulette',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://movie_roulette_user@localhost/movie_roulette',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-    }
+    
 }
