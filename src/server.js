1b78d97665b0efee0e6a06b5f9a87d7db238800e
Farah Gustafson <farah.gus@hotmail.com>
2021-07-12 17:16:51 -0400

    changes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config');
-const postgratorConfig = require('../postgrator-config');
-postgratorConfig.ssl = { rejectUnauthorized: false }
+const parse = require('pg-connection-string').parse;
+const pgconfig = parse('your-pg-connection-string');
+pgconfig.ssl = { rejectUnauthorized: false };
 
 const db = knex({
     client: "pg",
